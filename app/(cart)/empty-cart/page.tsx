5b571d4f2111b122419c936d4a06d3aaa75a81a2
Dev Johri <56020041+incruder1@users.noreply.github.com>
2024-03-15 23:56:43 +0530

    saD

--- app/(cart)/empty-cart/page.tsx
+++ app/(cart)/empty-cart/page.tsx
@@ -1,6 +1,6 @@
-// import Confirmation from "@/components/confirmation"
+import Confirmation from "@/component/confirmation"
 
-// const EmptyCartPage = () => {
-//     return <Confirmation imagePath="/bag.svg" heading="Your Bag is empty!" subheading="Add some items to proceed" buttonText="Add from watchlist" className="h-[50vh]" />
-// }
-// export default EmptyCartPage
+const EmptyCartPage = () => {
+    return <Confirmation imagePath="/bag.svg" heading="Your Bag is empty!" subheading="Add some items to proceed" buttonText="Add from watchlist" className="h-[50vh]" />
+}
+export default EmptyCartPage
